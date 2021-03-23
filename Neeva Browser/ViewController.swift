729b1d8f1962be4d67a108187aa5de80a1bf10d6
agent00097 <rushabhostwal@yahoo.in>
2021-03-23 02:15:03 -0400

    Basic Windows setup done

--- Neeva Browser/ViewController.swift
+++ Neeva Browser/ViewController.swift
@@ -2,16 +2,39 @@
 //  ViewController.swift
 //  Neeva Browser
 //
-//  Created by user929490 on 3/23/21.
+//  Created by agent97 on 3/23/21.
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var browserWindow: WKWebView!
+    
+    @IBAction func backButton(_ sender: Any) {
+        if browserWindow.canGoBack {
+            browserWindow.goBack()
+        }
+    }
+
+    @IBAction func reloadButton(_ sender: Any) {
+        browserWindow.reload()
+    }
+    @IBAction func forwardButton(_ sender: Any) {
+        if browserWindow.canGoForward {
+            browserWindow.goForward()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let defaultURL = URL(string: "https://mysterious-gorge-99398.herokuapp.com")!
+        
+        browserWindow.load(URLRequest(url: defaultURL))
+        
     }
 
 
